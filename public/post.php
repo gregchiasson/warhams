8a596ce6f1105a2b477c8e15827ccae3d63349ff
Greg Chiasson <gchiasson@emocha.com>
2018-07-01 21:56:29 -0400

    new list names

--- public/post.php
+++ public/post.php
@@ -12,7 +12,10 @@ ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE); 
 
 # your_list_[THIS].pdf
-$descriptors = array('sucks', 'is_puke', 'made_me_cry', 'blows', 'stinks', 'fucks', 'reeks', 'smells_like_an_unwashed_prostitute');
+$descriptors = array('sucks', 'is_puke', 'made_me_cry', 'blows', 'stinks', 'fucks', 'reeks', 'smells_like_an_unwashed_prostitute', 'is_shit',
+                     'is_shit', 'has_blood_of_champion', 'is_trash', 'is_garbage', 'look_like_a_dishrag', 'is_what_we_all_expected_from_you_really',
+                     '_is_disappoint'
+);
 
 function stat_block($stats, $rows) {
     reset($stats);
