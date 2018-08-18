c412bd1d954b2565cb06134b70a7c178f90531a8
Greg Chiasson <gchiasson@emocha.com>
2018-08-17 21:58:38 -0400

    readme changes

--- public/index.php
+++ public/index.php
@@ -15,14 +15,15 @@
     <li>Last update: <strong>14 August 2018</strong></li>
     <li><a href="https://github.com/gregchiasson/warhams">Feel free to submit PRs on GitHub</a></li>
     <li>Wrap text if it overflows on the Wound Tracker.</li>
-    <li>Added KillTeam support.</li>
+    <li>Add KillTeam support.</li>
 </ul>
 
 <h3>Known issues</h3>
 <ul>
-    <li>Some of the .rosz files don't want to unzip - added error detection and a workaround.</li>
+    <li>Some of the 40 .rosz files don't want to unzip - added error detection and a workaround.</li>
     <li>Mixed "soup" detachments are all beefed up.</li>
     <li>IG squads show up with "1 guardsman"</li>
+    <li>Kill Team is missing the "new guy" and "dead" check boxes</li>
 </ul>
 
 <h3>Credits</h3>
