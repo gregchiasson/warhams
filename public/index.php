1835ec0e93cae0b3e537c3bd8fdb165207db4a16
Greg Chiasson <gchiasson@emocha.com>
2019-04-29 22:34:48 -0400

    Update release notes

--- public/index.php
+++ public/index.php
@@ -38,15 +38,15 @@ Uncomment if there's known outages
     <div class="panel-body">
         <ul>
             <li>Last update: <strong>29 April 2019</strong></li>
-            <li>Updated UI.</li>
             <li><strong>BattleScribe 2.02+ works properly now.</strong></li>
+            <li>Updated UI.</li>
+            <li>Fix a bug where certain rules/abilities weren't appearing.</li>
             <li>Added URL to printouts so people can stop asking me where I printed these.</li>
             <li>Moved points cost to header, next to power level.</li>
             <li>Fix duplicate keywords appearing in some cases.</li>
         </ul>
     </div>
 </div>
-
 
 <div class="panel panel-danger">
     <div class="panel-heading">
@@ -55,11 +55,12 @@ Uncomment if there's known outages
     <div class="panel-body">
         <ul>
             <li>Some of the .rosz files don't want to unzip - added error detection and a workaround.</li>
-            <li>Mixed "soup" detachments are all beefed up.</li>
-            <li>IG squads show up with "1 guardsman".</li>
+            <li>Wound trackers/degrading statlines for vehicle squadrons (IG Hellhounds) don't want to work.</li>
+<!--
             <li>Kill Team is missing the "new guy" and "dead" check boxes.</li>
             <li>Kill Team cards get all beefed up if there's too many abilities</li>
             <li>Kill Team Campaign Roster doesn't work, only Kill Team Roster</li>
+-->
         </ul>
     </div>
 </div>
