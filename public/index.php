8536975c9f4f5be4557cc2777986402905e4765a
Greg Chiasson <gchiasson@emocha.com>
2021-04-17 23:49:30 -0400

    docs update

--- public/index.php
+++ public/index.php
@@ -26,7 +26,7 @@ Uncomment if there's known outages
             <li><a href="/40k.php">40k 9th Edition</a></li>
             <li><a href="/kt.php">Kill Team</a></li>
         </ul>  
-        <p>ButtScribe is a web application that runs off of BattleScribe output, and generates printable datasheets for the units in your army. It currently supports Warhammer 40k (9th Edition) and Kill-Team. The goal with ButtScribe was to bridge the gap in <em>printed materials</em>: the codices have nicely-formatted unit entries, but these are intended for selecting a unit's options, so it has a raft of information that might not be relevant depending on what options you took, and they don't include the actual points/PL costs of your specific unit. That is, the codex will have all of the wargear and rules for <em>a</em> Tactical Squad, but what you really want during a game is the wargear and rules for <em>your</em> Tactical squads, which is where ButtScribe comes in.</p>
+        <p>ButtScribe is a web application that runs off of BattleScribe output, and generates printable datasheets for the units in your army. It currently supports Warhammer 40k (9th Edition, including <strong>Crusade</strong>) and Kill-Team. The goal with ButtScribe was to bridge the gap in <em>printed materials</em>: the codices have nicely-formatted unit entries, but these are intended for selecting a unit's options, so it has a raft of information that might not be relevant depending on what options you took, and they don't include the actual points/PL costs of your specific unit. That is, the codex will have all of the wargear and rules for <em>a</em> Tactical Squad, but what you really want during a game is the wargear and rules for <em>your</em> Tactical squads, which is where ButtScribe comes in.</p>
         <p>Also, BattleScribe does, obviously, support printing army lists, as anyone who has ever played 40k can tell you. The problem is that those lists look like butt.</p>
         <p>The list of supported games is in the header up top, and if you run into any problems, feel free to <a href="mailto:contact@goonhammer.com">email me</a></p>
     </div>
@@ -38,7 +38,7 @@ Uncomment if there's known outages
     </div>
     <div class="panel-body">
         <ul>
-            <li>Latest update: <strong>20 April 2021</strong></li>
+            <li>Latest update: <strong>18 April 2021</strong></li>
             <li>Added support for Crusade rosters.</li>
         </ul>
     </div>
@@ -47,6 +47,11 @@ Uncomment if there's known outages
 <p>
     <a href="/output.png">
         <img src="/output.png" alt="Output data card example" width="700px"/>
+    </a>
+</p>
+<p>
+    <a href="/output.png">
+        <img src="/output_crusade.png" alt="Output crusade example" width="700px"/>
     </a>
 </p>
 <p>
