7201374260f968a539e2494890cedea9d85ab053
Greg Chiasson <gchiasson@emocha.com>
2023-07-31 11:41:52 -0400

    refactor: put the renderer and parser in their own libraries (not modules), and namespace them

---
+++ public/js/parse.js
@@ -0,0 +1,218 @@
+const buttParse = {
+  xmlToJson(xml) {
+    const obj = $.xml2json(xml);
+    var army = [];
+
+    console.log(obj);
+    
+    // soup
+    const forces = buttParse.forceArray(obj.roster.forces.force);
+    forces.forEach((force) => {
+      var list = {'cost': 0, 'rules': {}, 'cheat': {}, 'units': [], 'faction': 'Unknown', 'detachment': 'Unknown'};
+      list['faction'] = force['$'].catalogueName;
+      list['cost'] = `${parseInt(obj.roster.costs.cost['$'].value)}${obj.roster.costs.cost['$'].name}`;
+
+      if(force.rules) {
+        const rules = buttParse.forceArray(force.rules.rule);
+        rules.forEach((rule) => {
+          list['rules'][rule['$'].name] = rule.description;
+        });  
+      }
+
+      const selections = buttParse.forceArray(force.selections.selection);
+      selections.forEach((selection) => {
+        switch(selection['$'].type) {
+          case 'model':
+          case 'unit':
+            list['units'].push(buttParse.parseUnit(selection));
+            break;
+          case 'upgrade':
+            if(selection['$'].name == "Detachment" || selection['$'].name == "Detachment Choice") {
+              list['detachment'] = selection.selections.selection['$'].name;
+              if(selection.selections.selection.rules) {
+                const rules = buttParse.forceArray(selection.selections.selection.rules.rule);
+                rules.forEach((rule) => {
+                  list['rules'][rule['$'].name] = rule.description;
+                });  
+              }
+              if(selection.selections.selection.profiles) {
+                const rules = buttParse.forceArray(selection.selections.selection.profiles.profile);
+                rules.forEach((rule) => {
+                  list['rules'][rule['$'].name] = rule.characteristics.characteristic['_'];
+                });  
+              }
+
+            }
+            break;
+          case '':
+            break;
+        }
+      });
+
+      list['units'].forEach((unit) => {
+        Object.keys(unit['rules']).forEach((rule) => {
+          list['cheat'][rule] = unit['rules'][rule];
+        })
+      });
+      army.push(list);
+    });
+
+    return army;
+  },
+  parseUnit(selection) {
+    var unit = {
+      'sheet':    'unknown',
+      'points':    0,
+      'profiles':  {},
+      'abilities': {},
+      'rules':     {},
+      'keywords':  [],
+      'models':    [],
+      'wargear':   {},
+      'weapons':   {'ranged': {}, 'melee': {}}
+    };    
+    unit.sheet = selection['$'].name;
+
+    // model stat blocks and abilities
+    if(selection.profiles) {
+      const profiles = buttParse.forceArray(selection.profiles.profile);
+      profiles.forEach((profile) => {
+        unit = buttParse.parseProfile(unit, profile);
+      });  
+    }
+
+    // weapon stat blocks
+    const selections = buttParse.forceArray(selection.selections.selection);
+    selections.forEach((item) => {
+      if(item.profiles) {
+          const profiles = buttParse.forceArray(item.profiles.profile);
+          unit = buttParse.parseGuns(unit, profiles);
+      }
+      if(item.selections) {
+        const itemSelections = buttParse.forceArray(item.selections.selection);
+        itemSelections.forEach((gear) => {
+          if(gear.profiles) {
+            const profiles = buttParse.forceArray(gear.profiles.profile);
+            unit = buttParse.parseGuns(unit, profiles);  
+          } 
+          if(gear.selections) {
+            const fuckGuard = buttParse.forceArray(gear.selections.selection);
+            fuckGuard.forEach((fuckYou) => {
+              if(fuckYou.profiles) {
+                const profiles = buttParse.forceArray(fuckYou.profiles.profile);
+                unit = buttParse.parseGuns(unit, profiles);  
+              }     
+            });
+          }
+        });  
+      }
+    });
+
+    // unit points cost 
+    unit['points'] = parseInt(selection.costs.cost['$'].value);
+    const costSelections = buttParse.forceArray(selection.selections.selection);
+    costSelections.forEach((item) => {
+      unit['points'] += parseInt(item.costs.cost['$'].value);
+    });
+
+    // unit rules
+    if(selection.rules) {
+      const rules = buttParse.forceArray(selection.rules.rule);
+      rules.forEach((rule) => {
+        unit['rules'][rule['$'].name] = rule.description;
+      });  
+    }
+
+    // unit keywords (including faction, prefixed with "Faction: ")
+    const keywords = buttParse.forceArray(selection.categories.category);
+    keywords.forEach((keyword) => {
+      unit['keywords'].push(keyword['$'].name);
+    });
+
+    unit['keywords'].sort((a, b) => {
+      return a.match('Faction') ? -1 : 1;
+    });
+
+    // unit roster/model count
+    const models = buttParse.forceArray(selection.selections.selection);
+    var rawModels = {};
+    models.forEach((model) => {
+      if(model['$'].type == 'model') {
+        const modelName  = model['$'].name;
+        const modelCount = model['$'].number;
+        if(!rawModels[modelName]) {
+          rawModels[modelName] = 0;
+        }
+        rawModels[modelName] += parseInt(modelCount);  
+      }
+      // i love how i have to keep hacking this crap in because all the codex
+      // data maintainers are insane. this time, for guard infantry squads:
+      if(model.selections) {
+        const fuckGuard = buttParse.forceArray(model.selections.selection);
+        fuckGuard.forEach((fuckYou) => {
+          if(fuckYou['$'].type == 'model') {
+            const modelName  = fuckYou['$'].name;
+            const modelCount = fuckYou['$'].number;
+            if(!rawModels[modelName]) {
+              rawModels[modelName] = 0;
+            }
+            rawModels[modelName] += parseInt(modelCount);  
+          }
+        });    
+      }
+    });
+
+    if(Object.keys(rawModels).length > 0) {
+      Object.keys(rawModels).forEach((model) => {
+        unit['models'].push(`${rawModels[model]} ${model}`)
+      });  
+    } else {
+      unit['models'].push(`1 ${unit['sheet']}`)    
+    }
+
+    return unit;
+  }, 
+  parseGuns(unit, profiles) {
+    profiles.forEach((profile) => {
+      if(profile['$'].typeName == 'Ranged Weapons') {
+        unit['weapons']['ranged'][profile['$'].name] = buttParse.parseGun(profile);
+      } else if(profile['$'].typeName == 'Melee Weapons') {
+        unit['weapons']['melee'][profile['$'].name] = buttParse.parseGun(profile);
+      } else if(profile['$'].typeName == 'Abilities') {
+        unit['wargear'][profile['$'].name] = profile.characteristics.characteristic['_'];
+      }
+      unit = buttParse.parseProfile(unit, profile);
+    });
+    return unit;
+  },
+  parseProfile(unit, profile) {
+    if(profile['$'].typeName == 'Unit') {
+      const stats = profile.characteristics.characteristic;
+      var statblock = {};
+      stats.forEach((stat) => {
+        statblock[stat['$'].name] = stat['_'];
+      });
+      unit['profiles'][profile['$'].name] = statblock;
+    } else if(profile['$'].typeName == 'Abilities') {
+      unit['abilities'][profile['$'].name] = profile.characteristics.characteristic['_']; 
+    }
+    return unit;
+  },
+  parseGun(gun) {
+    var stats = {}
+    gun.characteristics.characteristic.forEach((stat) => {
+      stats[stat['$'].name] = stat['_'];
+    })
+    return stats;
+  },
+  forceArray(item) {
+    if(!item) { 
+      return [];
+    }
+    if(!Array.isArray(item)) {
+      return [item];
+    } else {
+      return item;
+    }
+  }
+};
---
+++ public/js/render.js
@@ -0,0 +1,220 @@
+const buttRender = {
+    jsonToHTML(json) {
+    var html = '';
+    json.forEach((force) => {
+      html += buttRender.renderCover(force);
+      html += buttRender.renderCheat(force);
+      html += buttRender.renderRules(force);
+      //html += buttRender.renderArmory(force);
+      html += buttRender.renderUnits(force['units']);
+    });
+    return html;
+  },
+  HTMLtoPDF(targetElement) {
+    var element = document.getElementById(targetElement);
+    html2pdf(element, {
+      filename:  'your_list_sucks.pdf',
+      pagebreak: { mode: 'css', after: '.page' },
+      margin:    0,
+      jsPDF: { format: 'letter', orientation: 'portrait' }
+    });
+  },
+  renderUnits(units) {
+    var content = '';
+    units.forEach((unit) => {
+      content += buttRender.renderUnit(unit);
+    });
+    return content;
+  },
+  renderCover(force) {
+    var unitRows = '';
+
+    force.units.sort((a, b) => {
+      return a.sheet.localeCompare(b.sheet);
+    });
+
+    force.units.forEach((unit) => {
+      unitRows += `<tr>
+      <td>${unit.sheet}</td>
+      <td>${unit.points}</td>
+      <td>${unit.models.join(', ')}</td>
+      </tr>`
+    });
+    return `<div id="coverPage" class="page">
+    <div class="row">
+    <div class="col-md-11 header">
+      <h3 class="floater">${force.detachment} Detachment</h3>
+      <h2>${force.faction} army - ${force.cost}</h2>
+    </div>
+    <div class="col-md-12">
+    <h4>Faction/Detachment Rules</h4>
+    <div class="rules">${buttRender.hashToLi(force.rules)}</div>
+    <hr/>
+    <h4>Units</h4>
+    <table class="table table-striped">
+      <thead><tr>
+        <th>Datasheet</th>
+        <th>Points</th>
+        <th>Models</th>
+      </tr></thead><tbody>
+      ${unitRows}
+    </tbody></table>
+    </div></div></div>`;
+  },
+  renderCheat(force) {
+    var unitData = '';
+
+    force.units.sort((a, b) => {
+      return a.sheet.localeCompare(b.sheet);
+    });
+
+    uniqueUnits = [];
+    seenUnits = [];
+    force.units.forEach((unit) => {
+      const hash = JSON.stringify(unit);
+      if(seenUnits.indexOf(hash) == -1) {
+        seenUnits.push(hash);
+        uniqueUnits.push(unit);
+      }
+    })
+
+    uniqueUnits.forEach((unit) => {
+      var allWeapons = unit.weapons['ranged'];
+      Object.keys(unit.weapons['melee']).forEach((gun) => {
+        allWeapons[gun] = unit.weapons['melee'][gun];
+      });
+      var allRules = unit.abilities;
+      Object.keys(unit.rules).forEach((rule) => {
+        allRules[rule] = unit.rules[rule];
+      })
+      unitData += `
+      <div class="col-md-6 unitSummary">
+      <h4>${unit.sheet}</h4>
+      <strong>Rules and Abilities</strong>: ${Object.keys(allRules).length ? Object.keys(allRules).sort().join(', ') : 'None'}
+      ${buttRender.makeTable(unit.profiles)}
+      ${buttRender.makeTable(allWeapons)}
+      </div>`;
+    });
+
+    return `
+    <div id="cheatPage" class="page"><div class="row">
+      <div class="col-md-11 header"><h2>Unit Reference</h2></div>
+        <div class="row">
+          ${unitData}
+        </div>
+      </div>
+    </div>`;
+  },
+  renderRules(force) {
+    var allRules = {};
+    force.units.forEach((unit) => {
+      Object.keys(unit.abilities).forEach((rule) => {
+        allRules[rule] = unit.abilities[rule];
+      })
+    });
+    return `
+    <div id="rulesPage" class="page"><div class="row">
+      <div class="col-md-11 header"><h2>Rules Reference</h2></div>
+        <div class="rules">${buttRender.hashToLi(allRules)}</div>
+      </div>
+    </div>`;
+
+  },
+  renderArmory(force) {
+    var allProfiles = {};
+    var allRangedWeapons = {};
+    var allMeleeWeapons = {};
+    force.units.forEach((unit) => {
+      Object.keys(unit.profiles).forEach((profile) => {
+        allProfiles[profile] = unit.profiles[profile];
+      })
+      Object.keys(unit.weapons['ranged']).forEach((profile) => {
+        const weaponName = `${unit.sheet}: ${profile}`;
+        allRangedWeapons[weaponName] = unit.weapons['ranged'][profile];
+      })
+      Object.keys(unit.weapons['melee']).forEach((profile) => {
+        const weaponName = `${unit.sheet}: ${profile}`;
+        allMeleeWeapons[weaponName] = unit.weapons['melee'][profile];
+      })
+    });
+    return `
+    <div id="refPage" class="page"><div class="row">
+    <div class="col-md-11 header"><h2>Rules Reference</h2></div>
+    <div class="row">
+    <div class="col-md-6">
+      <h4>Model Profiles</h4>
+      ${buttRender.makeTable(allProfiles)}
+      </div>
+      <div class="col-md-6">
+      <h4>Ranged Weapons</h4>
+      ${buttRender.makeTable(allRangedWeapons)}
+      <h4>Melee Weapons</h4>
+      ${buttRender.makeTable(allMeleeWeapons)}
+    </div>
+    </div>
+    </div>
+    </div>`;
+  },
+  renderUnit(unit) {
+    return `<div class="page">
+      <div class="row">
+      <div class="col-md-11 header">
+        <div class="floater">${unit.models.join(', ')}</div>
+        <h2>${unit.sheet} - ${unit.points} points</h2>
+        </div>
+      <div class="col-md-7">
+      ${buttRender.makeTable(unit.profiles)}
+      <h4>Ranged Weapons</h4>
+      ${buttRender.makeTable(unit.weapons['ranged'])}
+      <h4>Melee Weapons</h4>
+      ${buttRender.makeTable(unit.weapons['melee'])}
+      </div>
+      <div class="col-md-5">
+        <h4>Abilities</h4>
+        <div class="rules">${buttRender.hashToLi(unit.abilities)}</div>
+        <!--
+        <h4>Wargear</h4>
+        <div class="rules">${buttRender.hashToLi(unit.wargear)}</div>  
+        -->
+        <h4>Rules</h4>
+        <ul><li>${Object.keys(unit.rules).length ? Object.keys(unit.rules).sort().join(', ') : 'None'}</li></ul>
+        </div>
+      <div class="footer col-md-12"><strong>Keywords:</strong> ${unit.keywords.join(', ')}</div>
+    </div></div>
+    `;
+  },
+  makeTable(data) {
+    var content = '<table class="table table-striped"><thead>';
+
+    var first = true;
+    Object.keys(data).sort().forEach((row) => {
+      if(first == true) {
+        content += '<tr><th></th>';
+        Object.keys(data[row]).forEach((col) => {
+          content += `<th>${col}</th>`;
+        });
+        content += '</tr></thead><tbody>';  
+        first = false;
+      }
+      content += `<tr><th>${row}</th>`;
+      Object.keys(data[row]).forEach((col) => {
+        content += `<td>${data[row][col] || '-'}</td>`;
+      });
+      content += '</tr>';
+    });
+    content += '</tbody></table>'
+    return content;
+  },
+  hashToLi(items) {
+    var content = '<ul>';
+    if(Object.keys(items).length == 0) {
+      content += '<li>None</li>'
+    }
+    Object.keys(items).sort().forEach((item) => {
+      const formatted = items[item].replace(/\n+/g, '<br>');
+      content += `<li><strong>${item}:</strong> ${formatted}</li>`
+    });
+    content += '</ul>'
+    return content;
+  }
+}
